d9b620119f05cae847c30e0e7bd2fa8cb5bbbbe6
ArDesign <caleargan@gmail.com>
2017-02-11 04:16:04 -0800

    cleaned up, added functional buttons

--- script.js
+++ script.js
@@ -1,78 +1,89 @@
 $(document).ready(function(){
+    
+var x = 16;
+var grid = 0;
+var resetcolor = "blue";
+var currentcolor = "blue";
 
-var hoverreset = function () {
+
+var hoverreset = function(y) {
+    if (y === "blue") {
     var sq = $(".square");
-    sq.css("background-color","#4183D7");
-    sq.hover(function(){
-      $(this).css("background-color","#E4F1FE")
-    });
+    currentcolor = "blue";
+    sq.css("background-color","#4183d7");
+        sq.mouseover(function(){
+            $(this).css("background-color","#c5eff7")
+        });
+    } else if (y === "red") {
+    var sq = $(".square");
+    currentcolor = "red";
+    sq.css("background-color","#d91e18");
+        sq.mouseover(function(){
+            $(this).css("background-color","#f1a9a0");
+        });
+    } else if (y === "green") {
+    var sq = $(".square");
+    currentcolor = "green";
+    sq.css("background-color","#26a65b");
+        sq.mouseover(function(){
+            $(this).css("background-color","#66cc99");
+        });
+    }
+};
+    
+var makesize = function() {
+    if (x < 1 || x > 64 || isNaN(x) == true) {
+      alert("value is not a number between 4 and 64");
+    } else {
+      var width = 650 / x;
+      $(".square").css('width',width).css('height',width);
+      /*$(".square").css('height',width);*/
+        
+      multiplyboxes();
+      hoverreset(currentcolor);
+      
+    };
 };
 
 // random number:
 // "#"+((1<<24)*Math.random()|0).toString(16)
+var a = $(".square")
 
 var multiplyboxes = function() {
         grid = x * x - 1;
-        //var a = $(".square")
         for (var i=0; i<grid; i++) {
         a.clone().insertAfter(a);
         };
       };
 
-//initial grid size:
-var a = $(".square")
-        for (var i=0; i<255; i++) {
-        a.clone().insertAfter(a);
-        };
-
-var grid = 0;
-
-var width = 650 / 16;
-$(".square").css("width", width);
-$(".square").css("height", width);
-
-hoverreset();
-
-//multiply divs.
-//Use a class as it doesn't cause the same conflicts as IDs.
-//also put it in a variable before the loop
-/*var a = $(".square")
-    for (var i=0; i<255; i++) {
-        a.clone().insertAfter(a);
-    };
-    */
-    
-/*
-$(".square").hover(function(){
-    $(this).css("background-color","#FDE3A7");
-});
-*/
-
-var x = 0;
-
-$("#resetgrid").click(function() {
+//buttons:
+$("#choosesize").click(function() {
     x = prompt("Choose a grid size");
-    if (x < 1 || x > 64 || isNaN(x) == true) {
-      alert("value is not a number between 4 and 64");
-    } else {
-      
-      var width = 650 / x;
-      
-      $(".square").css('width',width);
-      $(".square").css('height',width);
-      
-      
-      multiplyboxes();
-      hoverreset();
-      
-    };
+    makesize();
+});
+    
+$("#resetgrid").click(function(){
+    x = 16;
+    multiplyboxes();
+    hoverreset(resetcolor);
+    makesize();
+});
+    
+$("#red").click(function(){
+    hoverreset("red");
+});
+$("#green").click(function(){
+    hoverreset("green");
+});
+$("#blue").click(function(){
+    hoverreset("blue");
 });
 
-/*grid = x * x;
-var a = $(".square")
-    for (var i=0; i<grid; i++) {
-        a.clone().insertAfter(a);
-    };*/
+//run functions
+makesize();
+multiplyboxes();
+hoverreset(resetcolor);
+
 
 }); //doc ready
 
